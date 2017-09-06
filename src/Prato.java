5143a9c0fe266f4bcd000aafec0099b04995cc9e
Marcelo Camargo Beus <marcelo.beus@gmail.com>
2017-09-06 15:30:59 -0300

    Gravando os arquivos no repositorio

---
+++ src/Prato.java
@@ -0,0 +1,31 @@
+
+public class Prato {
+	private String nome, descricao;
+	private double preco;
+
+	public Prato(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+}
