7a49b9064c166bb54412ea2ef3d4cac225bc1781
Marcelo Camargo Beus <marcelo.beus@gmail.com>
2017-09-13 18:01:59 -0300

    Adicionando classe Ingrediente e modificacoes Prato

---
+++ src/Ingrediente.java
@@ -0,0 +1,50 @@
+
+public class Ingrediente {
+	private String carne, legume, verdura, tempero, acompanhamento;
+
+	public Ingrediente(String legume, String verdura) {
+		this.legume = legume;
+		this.verdura = verdura;
+	}
+
+	public String getCarne() {
+		return carne;
+	}
+
+	public void setCarne(String carne) {
+		this.carne = carne;
+	}
+
+	public String getLegume() {
+		return legume;
+	}
+
+	public void setLegume(String legume) {
+		this.legume = legume;
+	}
+
+	public String getVerdura() {
+		return verdura;
+	}
+
+	public void setVerdura(String verdura) {
+		this.verdura = verdura;
+	}
+
+	public String getTempero() {
+		return tempero;
+	}
+
+	public void setTempero(String tempero) {
+		this.tempero = tempero;
+	}
+
+	public String getAcompanhamento() {
+		return acompanhamento;
+	}
+
+	public void setAcompanhamento(String acompanhamento) {
+		this.acompanhamento = acompanhamento;
+	}
+
+}
--- src/Prato.java
+++ src/Prato.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
 
 public class Prato {
 	private String nome, descricao;
 	private double preco;
+	private String legume = "legume", verdura = "verdura";
+	
+
 
 	public Prato(String nome, double preco) {
 		this.nome = nome;
 		this.preco = preco;
+		Ingrediente ingr = new Ingrediente(legume, verdura);
+		System.out.println("Contém legume e verdura, escolha o terceiro ingrediente: \n1 - Acompanhamento\n2 - Tempero\n3 - Carne");
+		int esc = new Scanner(System.in).nextInt();
+		if (esc == 1)
+			ingr.setAcompanhamento("Acompanhamento");
+		else if (esc == 2)
+			ingr.setTempero("Tempero");
+		else if (esc == 3)
+			ingr.setCarne("Carne");
+		else
+			System.out.println("Valor inválido");
+
 	}
 
 	public String getNome() {
