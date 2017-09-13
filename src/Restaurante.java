3cabe0da0095b83484eb333d596a82c56f0020e4
Marcelo Camargo Beus <marcelo.beus@gmail.com>
2017-09-13 12:49:49 -0300

    Adicionando alteracoes da classe Restaurante

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -1,11 +1,25 @@
+import java.util.*;
 
 public class Restaurante {
 
 	Prato pratos[] = new Prato[3];
 
+	public void geraPrato() {
+		for (int i = 0; i < pratos.length; i++) {
+			System.out.println("Digite o nome do Prato: ");
+			String nome = new Scanner(System.in).nextLine();
+			System.out.println("Digite o preço do Prato: ");
+			double preco = new Scanner(System.in).nextDouble();
+			System.out.println("Digite a descrição do Prato: ");
+			String descr = new Scanner(System.in).nextLine();
+			pratos[i] = new Prato(nome, preco);
+			pratos[i].setDescricao(descr);
+		}
+	}
+
 	public void mostrarCardapio() {
 		String mostra;
-		for (int i = 0; i <= 2; i++) {
+		for (int i = 0; i < pratos.length; i++) {
 			mostra = pratos[i].getNome() + ": " + pratos[i].getPreco() + "\n" + pratos[i].getDescricao();
 			System.out.println(mostra);
 
